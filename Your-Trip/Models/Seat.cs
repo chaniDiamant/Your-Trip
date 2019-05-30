80a8f7fcb0629ebee4a95fb4f29dfea48cbe327b
Tehila Rashkas <tehila.rashkes@cs.colman.ac.il>
2019-05-30 22:42:53 +0300

    Add Model

---
+++ Your-Trip/Models/Seat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourTrip.Models
+{
+    public class Seat
+    {
+        [DisplayName("מספר כיסא")]
+        public int SeatId { get; set; }
+
+        public Plane Plane { get; set; }
+
+        public Ticket Ticket { get; set; }
+    }
+}
