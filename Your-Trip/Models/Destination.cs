64274a46c001a69769f07f1a82661de88a031826
Hadar Bruchim <hadarb97@gmail.com>
2019-05-30 22:34:57 +0300

    open models

---
+++ Your-Trip/Models/Destination.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YourTrip.Models
+{
+    public class Destination
+    {
+        [DisplayName("מספר יעד")]
+        public int DestinationId { get; set; }
+
+        [DisplayName("שם")]
+        public string Name { get; set; }
+
+        [DisplayName("שפה")]
+        public string Language { get; set; }
+
+        [DisplayName("ערך מטבע")]
+        public string CurrencyValue { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
---
+++ Your-Trip/Models/Flight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YourTrip.Models
+{
+    public class Flight
+    {
+        [Key]
+        [DisplayName("מספר טיסה")]
+
+        public int FlightNumber { get; set; }
+
+        [DisplayName("תאריך המראה")]
+        public DateTime TakeOff { get; set; }
+
+        [DisplayName("תאריך נחיתה")]
+        public DateTime Landing { get; set; }
+
+        [DisplayName("מחיר")]
+        public float Price { get; set; }
+
+        public Terminal Terminal { get; set; }
+
+        public Destination Destination { get; set; }
+
+        public Plane Plane { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
